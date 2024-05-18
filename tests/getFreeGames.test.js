4c69a102331998971ca5b1fcc90696e6c5c3c0c3
Dundo <107737568+ganud@users.noreply.github.com>
2024-05-18 19:04:07 -0400

    Move tests into their own folder

---
+++ tests/getFreeGames.test.js
@@ -0,0 +1,10 @@
+const { getFreeGames, getTimeStamp } = require("../getFreeGames");
+const response = require("./response.json");
+require("jest-fetch-mock").enableMocks();
+
+it("Filters only games", async () => {
+  fetch.mockResponseOnce(JSON.stringify(response));
+  const games = await getFreeGames();
+  console.log(games);
+  expect(games.length).toEqual(1);
+});
