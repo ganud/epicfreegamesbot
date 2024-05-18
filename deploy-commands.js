5c4c7873fe18bfa21aad7500be580774b3b20312
Dundo <107737568+ganud@users.noreply.github.com>
2024-05-18 18:13:20 -0400

    Scope commands to be global

--- deploy-commands.js
+++ deploy-commands.js
@@ -38,11 +38,10 @@ const rest = new REST().setToken(token);
       `Started refreshing ${commands.length} application (/) commands.`
     );
 
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    );
+    // The put method is used to fully refresh all commands with the current set
+    const data = await rest.put(Routes.applicationCommands(clientId), {
+      body: commands,
+    });
 
     console.log(
       `Successfully reloaded ${data.length} application (/) commands.`
